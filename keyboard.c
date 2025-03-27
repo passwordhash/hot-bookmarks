e9d48bc4443efc8473f2dd0f7a9147bae9b5d445
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-03-27 19:14:31 +0300

    добавлена работа приложения в разных режимах

--- keyboard.c
+++ keyboard.c
@@ -1,7 +1,30 @@
 #include "keyboard.h"
+#include <strings.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_APP_PATH_LEN 512
+#define MAX_CMD_LEN 1024
 
 #define STR_LEN 10
+#define MAX_BINDS 10
+
+typedef struct
+{
+    int is_bound;                    // есть ли bind для цифры
+    char app_path[MAX_APP_PATH_LEN]; // путь к приложению или идентификатор окна
+} KeyBind;
+
+KeyBind binds[MAX_BINDS] = {0};
+
+typedef enum
+{
+    MODE_NORMAL,
+    MODE_BIND_ADD,
+    MODE_BIND_REMOVE
+} AppMode;
+
+AppMode current_mode = MODE_NORMAL;
 
 CGEventRef keyboard_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
 {
@@ -18,14 +41,53 @@ CGEventRef keyboard_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef
     UniCharCount pressed_str_len;
     CGEventKeyboardGetUnicodeString(event, STR_LEN, &pressed_str_len, pressed_str);
 
-    // проверяем, была ли использована клавиша Command + цифра
-    if (flags & kCGEventFlagMaskCommand)
+    // проверяем, была ли использована клавиша Command
+    if (!(flags & kCGEventFlagMaskCommand) || pressed_str_len != 1)
     {
-        if (pressed_str_len == 1 && pressed_str[0] >= '0' && pressed_str[0] <= '9')
+        return event;
+    }
+
+    char pressed = pressed_str[0];
+
+    if (pressed == 'x')
+    {
+        current_mode = MODE_BIND_ADD;
+        // TODO: logging
+        return event;
+    }
+
+    if (pressed == '-')
+    {
+        current_mode = MODE_BIND_REMOVE;
+        // TODO: logging
+        return event;
+    }
+
+    if (pressed >= '0' && pressed <= '9')
+    {
+        int index = pressed - '0';
+
+        if (current_mode == MODE_BIND_ADD)
         {
-            printf("command + %c pressed!\n", pressed_str[0]);
-            fflush(stdout);
+            // для теста добавляем калькулятор
+            char app_to_bind[MAX_APP_PATH_LEN] = "/Applications/Calculator.app";
+            strcpy(binds[index].app_path, app_to_bind);
+            // TODO: logging
         }
+        else if (current_mode == MODE_BIND_REMOVE)
+        {
+            binds[index].is_bound = 0;
+            binds[index].app_path[0] = '\0';
+        }
+        else if (current_mode == MODE_NORMAL && binds[index].is_bound)
+        {
+            char cmd[MAX_CMD_LEN];
+            snprintf(cmd, MAX_CMD_LEN, "open \"%s\"", binds[index].app_path);
+            system(cmd);
+            // TODO: logging
+        }
+
+        current_mode = MODE_NORMAL;
     }
 
     return event;
